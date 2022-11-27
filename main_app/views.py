9c135cbbf3c20d1db017e73d03bf0a859161481a
Fuad AlShuroogi <fuadalshuroogi@gmail.com>
2022-11-27 14:43:12 +0300

    Added finch show detail page

--- main_app/views.py
+++ main_app/views.py
@@ -17,17 +17,6 @@ def finches_index(request):
         finches = Finch.objects.all()
         return render(request, 'finches/index.html', { 'finches':finches })
 
-
-# # Add the Cat class & list and view function below the imports
-# class Finch:  # Note that parens are optional if not inheriting from another class
-#   def __init__(self, name, breed, description, age):
-#     self.name = name
-#     self.breed = breed
-#     self.description = description
-#     self.age = age
-
-# finches = [
-#   Finch('Lolo', 'tabby', 'foul little demon', 3),
-#   Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-#   Finch('Raven', 'black tripod', '3 legged cat', 4)
-# ]
+def finches_show(request, finch_id):
+        finch = Finch.objects.get(id=finch_id)
+        return render(request, 'finches/show.html', {'finch': finch})
