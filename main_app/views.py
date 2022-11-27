fb00da4f76e06b2b472cd3d4c6359c3244e51b72
Fuad AlShuroogi <fuadalshuroogi@gmail.com>
2022-11-27 14:00:18 +0300

    Connected Finch model to the view

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Finch
 
 # Create your views here.
-
-# function home(req,res)
 
 def home(request):
     return render(request, 'home.html')
@@ -14,16 +13,21 @@ def about(request):
 def finches_index(request):
   return render(request, 'finches/index.html', { 'finches': finches })
 
-# Add the Cat class & list and view function below the imports
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+def finches_index(request):
+        finches = Finch.objects.all()
+        return render(request, 'finches/index.html', { 'finches':finches })
 
-finches = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged cat', 4)
-]
+
+# # Add the Cat class & list and view function below the imports
+# class Finch:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
+
+# finches = [
+#   Finch('Lolo', 'tabby', 'foul little demon', 3),
+#   Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Finch('Raven', 'black tripod', '3 legged cat', 4)
+# ]
